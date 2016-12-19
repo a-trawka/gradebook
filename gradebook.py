8eb44468fbf4d7e03ef3b1b629a4a21c0c40cba2
Adrian <ad.trawka@gmail.com>
2016-12-19 21:50:30 +0100

    New teacher_required wrapper; logout() now clears whole session

--- gradebook.py
+++ gradebook.py
@@ -5,12 +5,16 @@ from flask import render_template
 from flask import request
 from flask import url_for
 from flask import session
+from flask import abort
 from peewee import *
 
+from wrappers import login_required, guest_status_required, teacher_required
+
 app = Flask(__name__)
-app.debug = False
-db = SqliteDatabase('gradebook.db')
+app.debug = True
 app.secret_key = 'gjk4*d/gaDc ;34d;Q'
+
+db = SqliteDatabase('gradebook.db')
 
 
 class BaseModel(Model):
@@ -21,16 +25,21 @@ class BaseModel(Model):
 class Subject(BaseModel):
     name = CharField()
 
-    class Meta:
-        order_by = ('name',)
-
 
 class Teacher(BaseModel):
     first_name = CharField()
     last_name = CharField()
-    specialization = ForeignKeyField(Subject, related_name='specializations')
     username = CharField(unique=True)
     password = CharField()
+
+
+# TeacherSubject, in other words - specializations
+# One teacher may teach many subjects,
+# as well as one subject may be taught by many teachers.
+# Many-to-many relationship model.
+class TeacherSubject(BaseModel):
+    teacher = ForeignKeyField(Teacher, related_name='teachers')
+    specialization = ForeignKeyField(Subject, related_name='specializations')
 
 
 class Student(BaseModel):
@@ -41,27 +50,41 @@ class Student(BaseModel):
     password = CharField()
 
 
-class Grades(BaseModel):
+class Grade(BaseModel):
     student = ForeignKeyField(Student)
     subject = ForeignKeyField(Subject)
     teacher = ForeignKeyField(Teacher)
     grade = CharField()
-    # grade_date = DateField()
 
     class Meta:
-        # TODO: ^add related_names ++ indexes here?
-        order_by = ('subject',)
+        order_by = ('student', 'subject', )
 
 
 def create_tables():
     db.connect()
-    db.create_tables([Student, Teacher, Subject, Grades], safe=True)
+    db.create_tables([Student, Teacher, Subject, TeacherSubject, Grade], safe=True)
 
 
-def authorize_student(stud):
+def authorize_student(student):
     session['logged_in'] = True
-    session['user_id'] = stud.id
-    session['username'] = stud.username
+    session['user_id'] = student.id
+    session['username'] = student.username
+    session['type'] = 'S'
+
+
+def authorize_teacher(teacher):
+    session['logged_in'] = True
+    session['user_id'] = teacher.id
+    session['username'] = teacher.username
+    session['type'] = 'T'
+
+
+def get_current_user():
+    if session['logged_in']:
+        if session['type'] == 'S':
+            return Student.get(Student.username == session['username'])
+        else:
+            return Teacher.get(Teacher.username == session['username'])
 
 
 @app.before_request
@@ -82,6 +105,7 @@ def homepage():
 
 
 @app.route('/new_student/', methods=(['GET', 'POST']))
+@teacher_required
 def new_student():
     if request.method == 'POST' and request.form['username']:
         try:
@@ -93,14 +117,15 @@ def new_student():
                     username=request.form['username'],
                     password=request.form['password'],
                 )
-        except EnvironmentError:
-            print('DEBUG: envir error')
-        return redirect(url_for('student_login'))
+        except DatabaseError:
+            abort(404)
+        return redirect(url_for('homepage'))
 
     return render_template('new_student.html')
 
 
 @app.route('/student_login/', methods=['GET', 'POST'])
+@guest_status_required
 def student_login():
     if request.method == 'POST' and request.form['username']:
         try:
@@ -109,13 +134,62 @@ def student_login():
                 password=request.form['password']
             )
         except Student.DoesNotExist:
-            # TODO: better exception catching
-            print("DEBUG: student does not exist")
+            abort(404)
         else:
             authorize_student(student)
-            # TODO: return redirect to some private page?
+            return redirect(url_for('student_profile'))
     return render_template('student_login.html')
 
+
+# TODO: student's profile accessible for teachers, but not for other students
+# student_profile/<username>
+@app.route('/student_profile/')
+@login_required
+def student_profile():
+    student = get_current_user()
+    return render_template('student_profile.html', student=student)
+
+
+# TODO: template for creating a teacher + adding TeacherSubject specialization!
+@app.route('/new_teacher/')
+def new_teacher():
+    return render_template('new_teacher.html')
+
+
+@app.route('/teacher_login/', methods=['GET', 'POST'])
+@guest_status_required
+def teacher_login():
+    if request.method == 'POST' and request.form['username']:
+        try:
+            teacher = Teacher.get(
+                username=request.form['username'],
+                password=request.form['password']
+            )
+        except Teacher.DoesNotExist:
+            abort(404)
+        else:
+            authorize_teacher(teacher)
+            return redirect(url_for('teacher_profile'))
+    return render_template('teacher_login.html')
+
+
+@app.route('/teacher_profile/')
+@login_required
+@teacher_required
+def teacher_profile():
+    teacher = get_current_user()
+    # or maybe: ...select().join(Teacher)...?
+    specs = TeacherSubject.select().where(TeacherSubject.teacher == teacher)
+    return render_template('teacher_profile.html', teacher=teacher, specializations=specs)
+
+
+@app.route('/logout/')
+@login_required
+def logout():
+    # This funcion clears all session variables
+    for field in session:
+        session[field] = None
+    return redirect(url_for('homepage'))
 
 if __name__ == '__main__':
     create_tables()
--- wrappers.py
+++ wrappers.py
@@ -11,13 +11,23 @@ def login_required(f):
     return func
 
 
-# wrapper for methods which cannot be accessed by a user who is already signed in (login, sign up)
+# wrapper for methods not accessible by users who are already logged in
 def guest_status_required(f):
     @wraps(f)
     def func(*args, **kwargs):
         if session.get('logged_in'):
             return redirect(url_for('homepage'))
         return f(*args, **kwargs)
+    return func
+
+
+# wrapper for methods accessible only by teachers
+def teacher_required(f):
+    @wraps(f)
+    def func(*args, **kwargs):
+        if session.get('type') == 'T':
+            return f(*args, **kwargs)
+        return redirect(url_for('homepage'))
     return func
 
 
