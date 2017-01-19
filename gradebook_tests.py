ea5a4c809630896cea68a214c8393f6619d1f475
Adrian <ad.trawka@gmail.com>
2017-01-19 12:14:10 +0100

    Minor change to testing module

--- gradebook_tests.py
+++ gradebook_tests.py
@@ -1,7 +1,5 @@
 import gradebook
 import unittest
-import tempfile
-import os
 
 
 class GradebookTestCase(unittest.TestCase):
@@ -10,9 +8,9 @@ class GradebookTestCase(unittest.TestCase):
         self.client = gradebook.app.test_client()
 
     def test_root(self):
-        """Tests '/' url, if its return value contains header Welcome"""
+        """Checks if '/' response data contains a header Welcome"""
         response = self.client.get('/')
-        assert b'<h1>Welcome</h1>' in response.data
+        self.assertIn(b'<h1>Welcome</h1>', response.data)
 
 if __name__ == '__main__':
     unittest.main()
