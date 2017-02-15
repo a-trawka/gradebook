1bc9d10e7006ebbc3267b290466a8872794688f9
Adrian <ad.trawka@gmail.com>
2017-02-15 19:58:19 +0100

    Started reworking admin into admin blueprint.

--- gradebook.py
+++ gradebook.py
@@ -14,12 +14,14 @@ from wrappers import teacher_required
 from wrappers import teacher_or_admin_required
 from wrappers import student_required
 from wrappers import admin_required
-from custom_exceptions import WrongPasswordException
 from db_model import *
+from custom_exceptions import WrongPasswordException
+from admin import admin_blueprint
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 app.config['SECRET_KEY'] = 'development'
+app.register_blueprint(admin_blueprint, url_prefix='/admin')
 
 db = get_db()
 
@@ -46,10 +48,10 @@ def authorize_teacher(teacher):
     session['username'] = teacher.username
     session['type'] = 'T'
 
-
-def authorize_admin():
-    session['logged_in'] = True
-    session['type'] = 'X'
+#ADMIN_OFF
+# def authorize_admin():
+#     session['logged_in'] = True
+#     session['type'] = 'X'
 
 
 def get_current_user():
@@ -79,28 +81,27 @@ def after_request(response):
 def homepage():
     return render_template('homepage.html')
 
-
-# TODO: password encryption
-@app.route('/new_student/', methods=(['GET', 'POST']))
-@admin_required
-def new_student():
-    if request.method == 'POST' and request.form['username'] and len(request.form['password']) <= 70:
-        try:
-            with db.transaction():
-                student = Student.create(
-                    first_name=request.form['first_name'],
-                    last_name=request.form['last_name'],
-                    group=request.form['group'],
-                    username=request.form['username'],
-                    password=hashpw(request.form['password'].encode('utf-8'), gensalt())
-                )
-        except IntegrityError:
-            flash('Username already taken')
-        except DatabaseError:
-            flash('An error occurred while creating a student')
-        else:
-            flash('Student added')
-    return render_template('new_student.html')
+#ADMIN_OFF
+# @app.route('/new_student/', methods=(['GET', 'POST']))
+# @admin_required
+# def new_student():
+#     if request.method == 'POST' and request.form['username'] and len(request.form['password']) <= 70:
+#         try:
+#             with db.transaction():
+#                 student = Student.create(
+#                     first_name=request.form['first_name'],
+#                     last_name=request.form['last_name'],
+#                     group=request.form['group'],
+#                     username=request.form['username'],
+#                     password=hashpw(request.form['password'].encode('utf-8'), gensalt())
+#                 )
+#         except IntegrityError:
+#             flash('Username already taken')
+#         except DatabaseError:
+#             flash('An error occurred while creating a student')
+#         else:
+#             flash('Student added')
+#     return render_template('new_student.html')
 
 
 @app.route('/student_login/', methods=['GET', 'POST'])
@@ -132,16 +133,24 @@ def student_profile():
     grades = Grade.select().where(Grade.student == student)
     return render_template('student_profile.html', student=student, subjects=subjects, grades=grades)
 
+# TODO: split this into student_profile_teacher and admin/student_profile with bp
+#ADMIN_OFF
+# teacher should be able to access every student's information
+# @app.route('/student_profile/<username>/')
+# @teacher_or_admin_required
+# def student_profile_foreign(username):
+#     student = Student.get(Student.username == username)
+#     subjects = Subject.select()
+#     grades = Grade.select().where(Grade.student == student)
+#     return render_template('student_profile.html', student=student, subjects=subjects, grades=grades)
 
-# teacher should be able to access every student's profile information
-@app.route('/student_profile/<username>/')
-@teacher_or_admin_required
+@app.route('/student_profile_foreign/<username>')
+@teacher_required
 def student_profile_foreign(username):
     student = Student.get(Student.username == username)
     subjects = Subject.select()
     grades = Grade.select().where(Grade.student == student)
     return render_template('student_profile.html', student=student, subjects=subjects, grades=grades)
-
 
 # TODO: validation of the form data
 @app.route('/add_grade/', methods=['GET', 'POST'])
@@ -165,27 +174,26 @@ def add_grade():
     subjects = Subject.select()
     return render_template('add_grade.html', students=students, subjects=subjects)
 
-
-@admin_required
-@app.route('/new_teacher/', methods=['GET', 'POST'])
-def new_teacher():
-    if request.method == 'POST' and request.form['username'] and len(request.form['password']) <= 70:
-        try:
-            with db.transaction():
-                teacher = Teacher.create(
-                    first_name=request.form['first_name'],
-                    last_name=request.form['last_name'],
-                    username=request.form['username'],
-                    password=hashpw(request.form['password'].encode('utf-8'), gensalt())
-                    #password=request.form['password']
-                )
-        except IntegrityError:
-            flash('Username already taken')
-        except DatabaseError:
-            flash('An error occurred while creating a teacher')
-        else:
-            flash('Teacher created')
-    return render_template('new_teacher.html')
+#ADMIN_OFF
+# @app.route('/new_teacher/', methods=['GET', 'POST'])
+# @admin_required
+# def new_teacher():
+#     if request.method == 'POST' and request.form['username'] and len(request.form['password']) <= 70:
+#         try:
+#             with db.transaction():
+#                 teacher = Teacher.create(
+#                     first_name=request.form['first_name'],
+#                     last_name=request.form['last_name'],
+#                     username=request.form['username'],
+#                     password=hashpw(request.form['password'].encode('utf-8'), gensalt())
+#                 )
+#         except IntegrityError:
+#             flash('Username already taken')
+#         except DatabaseError:
+#             flash('An error occurred while creating a teacher')
+#         else:
+#             flash('Teacher created')
+#     return render_template('new_teacher.html')
 
 
 @app.route('/teacher_login/', methods=['GET', 'POST'])
@@ -217,31 +225,33 @@ def teacher_profile():
     return render_template('teacher_profile.html', teacher=teacher, specializations=specs)
 
 
-@app.route('/teacher_profile/<username>/')
-@admin_required
-def teacher_profile_foreign(username):
-    teacher = Teacher.get(Teacher.username == username)
-    specs = TeacherSubject.select().where(TeacherSubject.teacher == teacher)
-    return render_template('teacher_profile.html', teacher=teacher, specializations=specs)
+#ADMIN_OFF
+# @app.route('/teacher_profile/<username>/')
+# @admin_required
+# def teacher_profile_foreign(username):
+#     teacher = Teacher.get(Teacher.username == username)
+#     specs = TeacherSubject.select().where(TeacherSubject.teacher == teacher)
+#     return render_template('teacher_profile.html', teacher=teacher, specializations=specs)
 
 
-@admin_required
-@app.route('/add_specialization/<username>/', methods=['GET', 'POST'])
-def add_specialization(username):
-    if request.method == 'POST':
-        try:
-            with db.transaction():
-                spec = TeacherSubject.create(
-                    teacher=Teacher.get(Teacher.username == username),
-                    specialization=Subject.get(Subject.name == request.form['subject_select'])
-                )
-        except DatabaseError:
-            flash('An error occurred, try again')
-        else:
-            flash('Specialization added')
-    subs = Subject.select()
-    t = Teacher.get(Teacher.username == username)
-    return render_template('add_specialization.html', teacher=t, subjects=subs)
+#ADMIN_OFF
+# @app.route('/add_specialization/<username>/', methods=['GET', 'POST'])
+# @admin_required
+# def add_specialization(username):
+#     if request.method == 'POST':
+#         try:
+#             with db.transaction():
+#                 spec = TeacherSubject.create(
+#                     teacher=Teacher.get(Teacher.username == username),
+#                     specialization=Subject.get(Subject.name == request.form['subject_select'])
+#                 )
+#         except DatabaseError:
+#             flash('An error occurred, try again')
+#         else:
+#             flash('Specialization added')
+#     subs = Subject.select()
+#     t = Teacher.get(Teacher.username == username)
+#     return render_template('add_specialization.html', teacher=t, subjects=subs)
 
 
 @app.route('/groups/')
@@ -266,55 +276,59 @@ def group_foreign(group_number):
     return render_template('group.html', group=group_number, students=students)
 
 
-@app.route('/add_subject/', methods=['GET', 'POST'])
-@admin_required
-def add_subject():
-    if request.method == 'POST':
-        try:
-            with db.transaction():
-                subject = Subject.create(name=request.form['name'])
-        except DatabaseError:
-            flash('An error occured, try again.')
-        else:
-            flash('Subject added.')
-    return render_template('add_subject.html')
+#ADMIN_OFF
+# @app.route('/add_subject/', methods=['GET', 'POST'])
+# @admin_required
+# def add_subject():
+#     if request.method == 'POST':
+#         try:
+#             with db.transaction():
+#                 subject = Subject.create(name=request.form['name'])
+#         except DatabaseError:
+#             flash('An error occured, try again.')
+#         else:
+#             flash('Subject added.')
+#     return render_template('add_subject.html')
 
+#ADMIN_OFF
+# @app.route('/admin_login/', methods=['GET', 'POST'])
+# @guest_status_required
+# def admin_login():
+#     if request.method == 'POST':
+#         if request.form['id'] == 'TEST' and request.form['pswd'] == 'TEST':
+#             authorize_admin()
+#             return redirect(url_for('admin'))
+#         else:
+#             flash('NOPE')
+#     return render_template('admin_login.html')
 
-@app.route('/admin_login/', methods=['GET', 'POST'])
-@guest_status_required
-def admin_login():
-    if request.method == 'POST':
-        if request.form['id'] == 'TEST' and request.form['pswd'] == 'TEST':
-            authorize_admin()
-            return redirect(url_for('admin'))
-        else:
-            flash('NOPE')
-    return render_template('admin_login.html')
+#ADMIN_OFF delete?
+# @app.route('/admin/')
+# @admin_required
+# def admin():
+#     return render_template('admin.html')
 
-
-@app.route('/admin/')
-@admin_required
-def admin():
-    return render_template('admin.html')
-
-
+#ADMIN_OFF
 # TODO: let admin edit users' credentials
-@app.route('/admin_students/')
-def admin_students():
-    students = Student.select()
-    return render_template('admin_students.html', students=students)
-
-
-@app.route('/admin_teachers/')
-def admin_teachers():
-    teachers = Teacher.select()
-    return render_template('admin_teachers.html', teachers=teachers)
-
-
-@app.route('/admin_subjects')
-def admin_subjects():
-    subjects = Subject.select()
-    return render_template('admin_subjects.html', subjects=subjects)
+# @app.route('/students/')
+# @admin_required
+# def admin_students():
+#     students = Student.select()
+#     return render_template('admin_students.html', students=students)
+#
+#
+# @app.route('/teachers/')
+# @admin_required
+# def admin_teachers():
+#     teachers = Teacher.select()
+#     return render_template('admin_teachers.html', teachers=teachers)
+#
+#
+# @app.route('/subjects')
+# @admin_required
+# def admin_subjects():
+#     subjects = Subject.select()
+#     return render_template('admin_subjects.html', subjects=subjects)
 
 
 @app.route('/logout/')
