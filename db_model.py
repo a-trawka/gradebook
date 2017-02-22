881693ed2138a67da3051513826e7917bad44951
Adrian <ad.trawka@gmail.com>
2017-02-22 15:28:05 +0100

    __repr__

--- db_model.py
+++ db_model.py
@@ -13,53 +13,49 @@ class BaseModel(Model):
 
 
 class Subject(BaseModel):
+    name = CharField()
+
     def __repr__(self):
         return self.name
 
-    name = CharField()
-
-
 class Teacher(BaseModel):
-    def __repr__(self):
-        return '{} {} - {}'.format(self.first_name, self.last_name, self.username)
-
     first_name = CharField()
     last_name = CharField()
     username = CharField(unique=True)
     password = CharField()
 
+    def __repr__(self):
+        return '{} {} - {}'.format(self.first_name, self.last_name, self.username)
 
 class TeacherSubject(BaseModel):
     """TeacherSubject, in other words - specializations
     One teacher can teach many subjects,
     as well as one subject can be taught by many teachers.
     Many-to-many relationship model."""
-    def __repr__(self):
-        return '{} taught by {}'.format(repr(self.specialization), repr(self.teacher))
-
     teacher = ForeignKeyField(Teacher, related_name='teachers')
     specialization = ForeignKeyField(Subject, related_name='specializations')
 
+    def __repr__(self):
+        return '{} taught by {}'.format(repr(self.specialization), repr(self.teacher))
 
 class Student(BaseModel):
-    def __repr__(self):
-        return '{} {}, group {} - {}'.format(self.first_name, self.last_name, self.group, self.username)
-
     first_name = CharField()
     last_name = CharField()
     group = CharField()
     username = CharField(unique=True)
     password = CharField()
 
+    def __repr__(self):
+        return '{} {}, group {} - {}'.format(self.first_name, self.last_name, self.group, self.username)
 
 class Grade(BaseModel):
-    def __repr__(self):
-        return 'student:{}, subject:{}, grade:{}, teacher:{}'.format(repr(self.student), repr(self.subject), self.grade, repr(self.teacher))
-
     student = ForeignKeyField(Student)
     subject = ForeignKeyField(Subject)
     teacher = ForeignKeyField(Teacher)
     grade = CharField()
+
+    def __repr__(self):
+        return 'student:{}, subject:{}, grade:{}, teacher:{}'.format(repr(self.student), repr(self.subject), self.grade, repr(self.teacher))
 
     class Meta:
         order_by = ('student', 'subject',)
